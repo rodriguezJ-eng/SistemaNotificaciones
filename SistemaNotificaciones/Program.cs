45036fb437201d86827068edbab8a0acc37e404c
rashelsanch1-star <rashelsanch1@gmail.com>
2026-05-01 19:32:29 -0600

    implementacion de NotificaciónSMS

--- SistemaNotificaciones/Program.cs
+++ SistemaNotificaciones/Program.cs
@@ -13,3 +13,21 @@ Notificacion correo = new NotificacionEmail(
     "saludo1");
 
 correo.Enviar();
+
+Notificacion smsValido = new NotificacionSMS(
+    "Hola, este es un mensaje SMS :)",
+    "88888888"
+);
+smsValido.Enviar();
+
+Notificacion smsNumeroInvalido = new NotificacionSMS(
+    "Mensaje corto",
+    "1234"
+);
+smsNumeroInvalido.Enviar();
+
+Notificacion smsMensajeLargo = new NotificacionSMS(
+    "Bueno esto se supone que debe ser un mensaje largo que tenga mas de 160 caracteres incluyendo espacios pero ya no se que tanto mas escribir asi que mejor hasta aca lo dejo.",
+    "88888888"
+);
+smsMensajeLargo.Enviar();
